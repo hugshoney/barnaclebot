6fc5ceabbaa29876e3fc76652ca127404d2d872f
hugshoney <honey@cumallover.me>
2020-07-05 16:18:55 +0700

    :pencil: add license header

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Barnacle Bot is Telegram bot that I use to help myself learn English while learning Go and vice versa. Sorry my Englis is still sucks, and my Go programming skill is horrible.
 package main
 
 import (
